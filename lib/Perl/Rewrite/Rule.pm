c34549616cc2562224070a8470facb0f8af38f98
Robert Rothenberg <rrwo@cpan.org>
2013-09-03 19:33:13 +0100

    Added comment about why PPI is not an attribute of Perl::Rewrite::Rule

--- lib/Perl/Rewrite/Rule.pm
+++ lib/Perl/Rewrite/Rule.pm
@@ -2,4 +2,8 @@ package Perl::Rewrite::Rule;
 
 use Any::Moose;
 
+# It's tempting to make PPI::Document an attribute of a rule, but we
+# want to instantiate the rules and run them through multiple
+# documents.
+
 1;
